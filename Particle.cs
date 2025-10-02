e7617ecf536c3722aaee115b5b62d221a6265b5d
Will Thorpe <Tho9268W@maidenerleghschool.co.uk>
2025-10-02 01:55:39 +0100

    Add project files.

---
+++ Particle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Fluid_Sim_0._4
+{
+    internal class Particle
+    {
+        private Vector2 pos;
+        private Vector2 vel;
+        private float influenceRad;
+
+        public Particle()
+        {
+
+        }
+
+        public void ParticleCollision()
+        {
+
+        }
+        public void ObjectCollisionCheck()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+    }
+}
